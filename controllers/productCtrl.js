05f50722aeeb8eb96642e792b7c102932ae6c6b5
Sam <samb283@gmail.com>
2017-10-23 13:23:03 -0500

    trying to get product types back

--- controllers/productCtrl.js
+++ controllers/productCtrl.js
@@ -2,5 +2,9 @@
 
 // display form for creating new product
 module.exports.displayProductAdd = (req, res, next) => {
-  res.render('add-product');
+  const { ProductType } = req.app.get('models');
+  ProductType.findAll().then(data => {
+    console.log('my god damn product data', data);
+    res.render('add-product', data);
+  });
 };
