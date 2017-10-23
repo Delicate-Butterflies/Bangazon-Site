3fa559014f5d2d66e308ae83433f20b743850070
Megan B <brown.m.j@outlook.com>
2017-10-23 18:22:54 -0500

    work on getting count for products

--- controllers/productTypeCtrl.js
+++ controllers/productTypeCtrl.js
@@ -1,18 +1,42 @@
 'use strict';
 
+const sequelize = require('sequelize');
+
 /** @module Product Type Controller */
 
 module.exports.getAllProductTypes = (req, res, next) => {
   const { ProductType, Product } = req.app.get('models');
   ProductType.findAll({
-    include: [{ model: Product, limit: 3 }]
+    include: [
+      {
+        model: Product,
+        duplicating: false,
+        attributes: [[sequelize.fn('COUNT', sequelize.col('Product.id')), 'totalProducts'], 'Product'],
+        group: ['Product.productTypeId']
+      }
+    ]
   })
     .then(results => {
-      // res.json(results);
-      let productTypes = results;
-      res.render('product-types', { productTypes });
+      res.json(results);
+      // let productTypes = results;
+      // res.render('product-types', { productTypes });
     })
     .catch(err => {
       next(err);
     });
 };
+
+// module.exports.getAllProductTypes = (req, res, next) => {
+//   const { Product } = req.app.get('models');
+//   Product.count({
+//     col: 'Product.id',
+//     distinct: true,
+//     group: 'Product.productTypeId'
+//   })
+//     .then(results => {
+//       res.json(results);
+//     })
+//     .catch(err => {
+//       next(err);
+//     });
+// };
