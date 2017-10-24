bfa645aea9376c82301a77612e25f8630b977e52
Megan B <brown.m.j@outlook.com>
2017-10-23 20:25:09 -0500

    ticket complete except for count

--- controllers/productTypeCtrl.js
+++ controllers/productTypeCtrl.js
@@ -10,33 +10,15 @@ module.exports.getAllProductTypes = (req, res, next) => {
     include: [
       {
         model: Product,
-        duplicating: false,
-        attributes: [[sequelize.fn('COUNT', sequelize.col('Product.id')), 'totalProducts'], 'Product'],
-        group: ['Product.productTypeId']
+        limit: 3
       }
     ]
   })
     .then(results => {
-      res.json(results);
-      // let productTypes = results;
-      // res.render('product-types', { productTypes });
+      let productTypes = results;
+      res.render('product-types', { productTypes });
     })
     .catch(err => {
       next(err);
     });
 };
-
-// module.exports.getAllProductTypes = (req, res, next) => {
-//   const { Product } = req.app.get('models');
-//   Product.count({
-//     col: 'Product.id',
-//     distinct: true,
-//     group: 'Product.productTypeId'
-//   })
-//     .then(results => {
-//       res.json(results);
-//     })
-//     .catch(err => {
-//       next(err);
-//     });
-// };
