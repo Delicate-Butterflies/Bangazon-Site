e946fafabc064b8669b8facf8f5a3fc31c66ae8a
Megan B <brown.m.j@outlook.com>
2017-10-24 11:54:37 -0500

    jsdocs and clarifying comments

--- controllers/productTypeCtrl.js
+++ controllers/productTypeCtrl.js
@@ -4,28 +4,9 @@ const sequelize = require('sequelize');
 
 /** @module Product Type Controller */
 
-// module.exports.getAllProductTypes = (req, res, next) => {
-//   const { ProductType, Product } = req.app.get('models');
-//   ProductType.findAll({
-//     include: [
-//       {
-//         model: Product,
-//         attributes: []
-//       }
-//     ],
-//     attributes: {
-//       include: [[sequelize.fn('COUNT', sequelize.col('Products.id')), 'productCount']]
-//     },
-//     group: ['ProductType.id']
-//   })
-//     .then(productTypes => {
-//       res.json(productTypes);
-//       // res.render('product-types', { productTypes });
-//     })
-//     .catch(err => {
-//       next(err);
-//     });
-// };
+/**
+ * Get all product types and associated products and render 'product-types'
+ */
 
 module.exports.getAllProductTypes = (req, res, next) => {
   const { ProductType, Product } = req.app.get('models');
