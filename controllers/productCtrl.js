0aa62878a197fb6fc9585c6512bb5ebd36895b1d
thejonroberts <jon.roberts.8@gmail.com>
2017-10-23 23:54:34 -0500

    comment out include - quantity remaining is WIP

--- controllers/productCtrl.js
+++ controllers/productCtrl.js
@@ -8,20 +8,20 @@
 module.exports.getProductById = (req, res, next) => {
 	const { Product, Order } = req.app.get('models');
 	Product.findById(req.params.id, {
-		include: [
-			{
-				model: Order,
-				where: {
-					PaymentTypeId: {
-						$ne: null
-					}
-				}
-			}
-		]
+		// include: [
+		// 	{
+		// 		model: Order,
+		// 		where: {
+		// 			PaymentTypeId: {
+		// 				$ne: null
+		// 			}
+		// 		}
+		// 	}
+		// ]
 	})
 		.then(product => {
-			res.json(product);
-			// res.render('product-details', { product });
+			// res.json(product);
+			res.render('product-details', { product });
 		})
 		.catch(err => {
 			next(err);
