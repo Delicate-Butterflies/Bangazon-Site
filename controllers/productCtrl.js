3c0a675a2e37ddd8a77375b6301b6d8478bc3760
Sam <samb283@gmail.com>
2017-10-24 15:20:16 -0500

    input validation implemented for form

--- controllers/productCtrl.js
+++ controllers/productCtrl.js
@@ -21,37 +21,60 @@ module.exports.createNewProduct = (req, res, next) => {
   req.body.sellerUserId = req.session.passport.user.id;
   const { Product } = req.app.get('models');
 
-  // req.checkBody('title', 'Last Name must be alphanumeric.').isAlpha();
+  // Validate req.body information
+  req.checkBody('title', 'Title must be alphanumeric.').isAlpha();
+  req.checkBody('price', 'Price must be a decimal').isNumeric();
+  req.checkBody('description', 'Description must be alphanumeric').isAlpha();
+  req.checkBody('ProductTypeId', 'Product Type must be an integer').isInt();
+  req.checkBody('quantity', 'Quantity must be an integer').isInt();
+  req.checkBody('pictureUrl', 'ImageURL must be a URL').isURL();
 
-  // const errors = req.validationErrors();
+  // Sanitize req.body
+  // Escape html characters
+  req.checkBody('title').escape();
+  req.checkBody('price').escape();
+  req.checkBody('description').escape();
+  req.checkBody('ProductTypeId').escape();
+  req.checkBody('quantity').escape();
+  req.checkBody('pictureUrl').escape();
+  // Trim leading and trailing whitespace
+  req.checkBody('title').trim();
+  req.checkBody('price').trim();
+  req.checkBody('description').trim();
+  req.checkBody('ProductTypeId').trim();
+  req.checkBody('quantity').trim();
+  req.checkBody('pictureUrl').trim();
 
-  // const product = new Product(req.body);
-  // console.log(errors);
+  const errors = req.validationErrors();
+  const product = new Product(req.body);
 
-  // if (errors) {
-  //   //If there are errors render the form again, passing the previously entered values and errors
-  //   const { ProductType } = req.app.get('models');
-  //   ProductType.findAll().then(data => {
-  //     data.productTypes = data.map(trainee => {
-  //       return Object.assign({}, trainee.dataValues);
-  //     });
-  //     res.render('add-product', data, { title: 'Create Product', product: product, errors: errors });
-  //   });
-  //   return;
-  // } else {
-  Product.create(req.body, {
-    'req.body.title': {
-      validate: { isAlpha: true }
-    }
-  })
-    .then(response => {
-      // console.log('RESPONSE FROM THE POST', response);
-      res.redirect(`/products/${response.dataValues.id}`);
-    })
-    .catch(err => {
-      next(err);
+  if (errors) {
+    //If there are errors render the form again, passing the previously entered values and errors
+    const { ProductType } = req.app.get('models');
+    ProductType.findAll().then(data => {
+      data.productTypes = data.map(trainee => {
+        return Object.assign({}, trainee.dataValues);
+      });
+      data.product = product;
+      data.errors = errors;
+      console.log(data);
+      res.render('add-product', data);
     });
-  // }
+    return;
+  } else {
+    Product.create(req.body, {
+      'req.body.title': {
+        validate: { isAlpha: true }
+      }
+    })
+      .then(response => {
+        // console.log('RESPONSE FROM THE POST', response);
+        res.redirect(`/products/${response.dataValues.id}`);
+      })
+      .catch(err => {
+        next(err);
+      });
+  }
 };
 
 module.exports.getProductById = (req, res, next) => {
