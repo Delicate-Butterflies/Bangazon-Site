b4c3b4e4f7016520543b9285430c20a2ac169828
Arwa <arwa.it15@gmail.com>
2017-10-24 15:59:09 -0500

    bug to solve no payment

--- migrations/20171023170027-create-order-product.js
+++ migrations/20171023170027-create-order-product.js
@@ -1,7 +1,6 @@
 'use strict';
 
 module.exports = {
-<<<<<<< HEAD
   up: (queryInterface, Sequelize) => {
     return queryInterface.createTable('OrdersProducts', {
       id: {
@@ -41,49 +40,8 @@ module.exports = {
       }
     });
   },
-=======
-	up: (queryInterface, Sequelize) => {
-		return queryInterface.createTable('OrdersProducts', {
-			id: {
-				allowNull: false,
-				autoIncrement: true,
-				primaryKey: true,
-				type: Sequelize.INTEGER
-			},
-			OrderId: {
-				type: Sequelize.INTEGER,
-				allowNull: false,
-				references: {
-					// Again, note the plural!!!!!
-					model: 'Orders',
-					key: 'id'
-				},
-				onUpdate: 'CASCADE',
-				onDelete: 'CASCADE'
-			},
-			ProductId: {
-				type: Sequelize.INTEGER,
-				allowNull: false,
-				references: {
-					model: 'Products',
-					key: 'id'
-				},
-				onUpdate: 'CASCADE',
-				onDelete: 'CASCADE'
-			},
-			createdAt: {
-				allowNull: false,
-				type: Sequelize.DATE
-			},
-			updatedAt: {
-				allowNull: false,
-				type: Sequelize.DATE
-			}
-		});
-	},
->>>>>>> master
 
-	down: (queryInterface, Sequelize) => {
-		return queryInterface.dropTable('OrdersProducts');
-	}
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('OrdersProducts');
+  }
 };
