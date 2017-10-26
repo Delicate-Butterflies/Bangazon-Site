335e8c43036cb33a2eee007522d95c36eb6f950b
Megan B <brown.m.j@outlook.com>
2017-10-26 12:08:33 -0500

    user can edit account details

--- controllers/userAccountCtrl.js
+++ controllers/userAccountCtrl.js
@@ -5,9 +5,8 @@
 /**
  * Get the current user's account information
  */
-
 module.exports.getCurrentUserAccountDetails = (req, res, next) => {
-  const { User, PaymentType, Order } = req.app.get('models');
+  const { User } = req.app.get('models');
   let activeUserId = req.session.passport.user.id; //current active user
   User.findById(activeUserId, {})
     .then(user => {
@@ -16,4 +15,41 @@ module.exports.getCurrentUserAccountDetails = (req, res, next) => {
     .catch(err => {
       next(err);
     });
+};
+
+/**
+ * Get the current user's account information and render form
+ */
+module.exports.showEditAccountForm = (req, res, next) => {
+  const { User } = req.app.get('models');
+  let activeUserId = req.session.passport.user.id; //current active user
+  User.findById(activeUserId, {})
+    .then(user => {
+      res.render('edit-account', { user });
+    })
+    .catch(err => {
+      next(err);
+    });
+};
+
+module.exports.editUserAccount = (req, res, next) => {
+  const { User } = req.app.get('models');
+  let activeUserId = req.session.passport.user.id; //current active user
+  let { firstName, lastName, username, email, phoneNumber, streetAddress, cityAddress, stateCode, zipCode } = req.body;
+  User.update(req.body, {
+    where: { id: activeUserId },
+    fields: {
+      firstName,
+      lastName,
+      username,
+      email,
+      phoneNumber,
+      streetAddress,
+      cityAddress,
+      stateCode,
+      zipCode
+    }
+  }).then(user => {
+    res.redirect('/account');
+  });
 };
--- routes/user-account.js
+++ routes/user-account.js
@@ -3,9 +3,15 @@
 const { Router } = require('express');
 const router = Router();
 
-const { getCurrentUserAccountDetails } = require('../controllers/userAccountCtrl');
+const {
+  getCurrentUserAccountDetails,
+  showEditAccountForm,
+  editUserAccount
+} = require('../controllers/userAccountCtrl');
 
 router.get('/account', isLoggedIn, getCurrentUserAccountDetails);
+router.get('/edit-account', isLoggedIn, showEditAccountForm);
+router.post('/edit-account', isLoggedIn, editUserAccount);
 
 function isLoggedIn(req, res, next) {
   if (req.isAuthenticated()) return next();
