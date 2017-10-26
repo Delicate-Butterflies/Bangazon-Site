471f2a33d0a5abe774a31bab3892c5d71c051506
Megan B <brown.m.j@outlook.com>
2017-10-25 19:04:50 -0500

    user can see account details, payment types and order history on account view

--- controllers/userAccountCtrl.js
+++ controllers/userAccountCtrl.js
@@ -6,36 +6,6 @@
  * Get the current user's account information
  */
 
-// module.exports.getCurrentUserAccount = (req, res, next) => {
-//   const { User, PaymentType } = req.app.get('models');
-//   User.findById(req.params.id, {})
-//     .then(user => {
-//       // res.json(user);
-//       res.render('user-account', { user });
-//     })
-//     .catch(err => {
-//       next(err);
-//     });
-// };
-
-// module.exports.getUserPaymentTypes = (req, res, next, renderPage) => {
-//   let activeUserId = req.session.passport.user.id; //current active user
-//   const { PaymentType } = req.app.get('models');
-//   PaymentType.findAll({
-//     where: { customerUserId: activeUserId }
-//   })
-//     .then(data => {
-//       // res.json(data);
-//       if (data.length > 0) {
-//         let paymentTypes = data;
-//         res.render('user-account', { paymentTypes });
-//       } else res.render('user-account', data[0]);
-//     })
-//     .catch(err => {
-//       next(err);
-//     });
-// };
-
 module.exports.getCurrentUserAccountDetails = (req, res, next) => {
   const { User, PaymentType, Order } = req.app.get('models');
   const userDetails = {};
@@ -54,8 +24,8 @@ module.exports.getCurrentUserAccountDetails = (req, res, next) => {
       })
         .then(orders => {
           userDetails.orders = orders;
-          // res.render('user-account', { userDetails });
-          res.json(userDetails);
+          res.render('user-account', { userDetails });
+          // res.json(userDetails);
         })
         .catch(err => {
           next(err);
