66b54881340852fffb1f447ad0af65e963329460
Arwa <arwa.it15@gmail.com>
2017-10-27 15:15:36 -0500

    merge conflict

--- controllers/orderCtrl.js
+++ controllers/orderCtrl.js
@@ -50,14 +50,14 @@ module.exports.getOpenOrder = (req, res, next) => {
  * deletes the order by id. Helper function.
  */
 let deleteOrder = (req, res, next) => {
-	const { Order } = req.app.get('models');
-	Order.destroy({ where: { id: currentOrder[0].id } })
-		.then(() => {
-			res.redirect('/cart');
-		})
-		.catch(err => {
-			next(err);
-		});
+  const { Order } = req.app.get('models');
+  Order.destroy({ where: { id: currentOrder[0].id } })
+    .then(() => {
+      res.redirect('/cart');
+    })
+    .catch(err => {
+      next(err);
+    });
 };
 
 /**
@@ -65,38 +65,38 @@ let deleteOrder = (req, res, next) => {
  */
 
 module.exports.savePaymentType = (req, res, next) => {
-	const { Order } = req.app.get('models');
-	Order.update(
-		{ PaymentTypeId: req.body.paymentId },
-		{
-			where: { id: currentOrder[0].id }
-		}
-	)
-		.then(() => {
-			res.render('payment-confirmation');
-		})
-		.catch(err => {
-			next(err);
-		});
+  const { Order } = req.app.get('models');
+  Order.update(
+    { PaymentTypeId: req.body.paymentId },
+    {
+      where: { id: currentOrder[0].id }
+    }
+  )
+    .then(() => {
+      res.render('payment-confirmation');
+    })
+    .catch(err => {
+      next(err);
+    });
 };
 
 /**
  * removes products from the cart or the current open order!
  */
 module.exports.removeProductFromCart = (req, res, next) => {
-	const { Order, Product } = req.app.get('models');
-	Order.findById(currentOrder[0].id, {
-		include: [{ model: Product }]
-	})
-		.then(cart => {
-			cart.removeProducts(req.params.productId);
-		})
-		.then(() => {
-			res.redirect('/cart');
-		})
-		.catch(err => {
-			next(err);
-		});
+  const { Order, Product } = req.app.get('models');
+  Order.findById(currentOrder[0].id, {
+    include: [{ model: Product }]
+  })
+    .then(cart => {
+      cart.removeProducts(req.params.productId);
+    })
+    .then(() => {
+      res.redirect('/cart');
+    })
+    .catch(err => {
+      next(err);
+    });
 };
 
 /**
@@ -104,77 +104,76 @@ module.exports.removeProductFromCart = (req, res, next) => {
    */
 
 module.exports.cancelOrder = (req, res, next) => {
-	const { Order, Product } = req.app.get('models');
-	Order.findById(currentOrder[0].id, {
-		include: [{ model: Product }]
-	})
-		.then(cart => {
-			cart.Products.forEach(product => {
-				cart.removeProducts(product.id);
-			});
-		})
-		.then(() => {
-			deleteOrder(req, res, next);
-		})
-		.catch(err => {
-			next(err);
-		});
+  const { Order, Product } = req.app.get('models');
+  Order.findById(currentOrder[0].id, {
+    include: [{ model: Product }]
+  })
+    .then(cart => {
+      cart.Products.forEach(product => {
+        cart.removeProducts(product.id);
+      });
+    })
+    .then(() => {
+      deleteOrder(req, res, next);
+    })
+    .catch(err => {
+      next(err);
+    });
 };
 
 /**
 * Get order details including products and total price
 */
 module.exports.getUserOrderHistory = (req, res, next) => {
-	const { Order } = req.app.get('models');
-	let activeUserId = req.session.passport.user.id; //get current active user
-	Order.findAll({
-		where: {
-			customerUserId: activeUserId,
-			$PaymentTypeId$: { $ne: null }
-		}
-	})
-		.then(orders => {
-			res.render('order-history', { orders });
-		})
-		.catch(err => {
-			next(err);
-		});
+  const { Order } = req.app.get('models');
+  let activeUserId = req.session.passport.user.id; //get current active user
+  Order.findAll({
+    where: {
+      customerUserId: activeUserId,
+      $PaymentTypeId$: { $ne: null }
+    }
+  })
+    .then(orders => {
+      res.render('order-history', { orders });
+    })
+    .catch(err => {
+      next(err);
+    });
 };
 
 /**
 * Get order details including products and total price
 */
 module.exports.getUserOrderDetails = (req, res, next) => {
-	const { Order, Product, sequelize } = req.app.get('models');
-	Order.findAll({
-		include: [{ model: Product }],
-		where: { id: req.params.id }
-	}).then(results => {
-		let orderDetails = results[0];
-		if (results[0]) {
-			sequelize // raw query to count products in open order.
-				.query(
-					`SELECT  "ProductId", COUNT(*) as "productCount" FROM "OrdersProducts" WHERE "OrderId" = ${results[0]
-						.id} GROUP BY "ProductId"`,
-					{
-						type: sequelize.QueryTypes.SELECT
-					}
-				)
-				.then(counts => {
-					res.render('order-details', { orderDetails, counts });
-				})
-				.catch(err => {
-					next(err);
-				});
-		}
-	});
+  const { Order, Product, sequelize } = req.app.get('models');
+  Order.findAll({
+    include: [{ model: Product }],
+    where: { id: req.params.id }
+  }).then(results => {
+    let orderDetails = results[0];
+    if (results[0]) {
+      sequelize // raw query to count products in open order.
+        .query(
+          `SELECT  "ProductId", COUNT(*) as "productCount" FROM "OrdersProducts" WHERE "OrderId" = ${results[0]
+            .id} GROUP BY "ProductId"`,
+          {
+            type: sequelize.QueryTypes.SELECT
+          }
+        )
+        .then(counts => {
+          res.render('order-details', { orderDetails, counts });
+        })
+        .catch(err => {
+          next(err);
+        });
+    }
+  });
 };
 
 /**
  * add new products to the cart
  */
 module.exports.addProductToCart = (req, res, next) => {
-<<<<<<< HEAD
   const { Order, Product } = req.app.get('models');
   let activeUserId = req.session.passport.user.id; //get current active user
   Order.findAll({
@@ -198,46 +197,22 @@ module.exports.addProductToCart = (req, res, next) => {
     .catch(err => {
       next(err);
     });
-=======
-	const { Order, Product } = req.app.get('models');
-	let activeUserId = req.session.passport.user.id; //get current active user
-	Order.findAll({
-		include: [{ model: Product }],
-		where: { customerUserId: activeUserId, PaymentTypeId: null }
-	})
-		.then(theOrder => {
-			if (theOrder[0]) {
-				// if there is an open order
-				Order.findById(theOrder[0].id, {
-					include: [{ model: Product }]
-				}).then(currentOrder => {
-					currentOrder.addProducts(req.params.productId);
-					res.redirect('/cart');
-				});
-			} else {
-				createOrder(req, res, next); // if there is no open order, call createOrder
-			}
-		})
-		.catch(err => {
-			next(err);
-		});
->>>>>>> master
 };
 
 /**
  * helper function to create an order if there are no current open order.
  */
 let createOrder = (req, res, next) => {
-	let activeUserId = req.session.passport.user.id; //get current active user
-	const { Order } = req.app.get('models');
-	let orderObj = {
-		orderDate: new Date().toISOString(),
-		PaymentTypeId: null,
-		customerUserId: activeUserId
-	};
-	Order.create(orderObj).then(() => {
-		module.exports.addProductToCart(req, res, next);
-	});
+  let activeUserId = req.session.passport.user.id; //get current active user
+  const { Order } = req.app.get('models');
+  let orderObj = {
+    orderDate: new Date().toISOString(),
+    PaymentTypeId: null,
+    customerUserId: activeUserId
+  };
+  Order.create(orderObj).then(() => {
+    module.exports.addProductToCart(req, res, next);
+  });
 };
 
 /**
